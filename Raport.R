89b1bab456b263013386c6024bdf915bd8c242fd
Wojciech Jankowiak <jojtekwankowiak@gmail.com>
2020-12-11 20:12:18 +0100

    poprawka

--- Raport.R
+++ Raport.R
@@ -16,41 +16,41 @@ summary(dat)
 
 
 # Rok 2020
-#dat2020 = get_eurostat("tps00002", type = "code", filters = list(time = 2020), time_format = "num")
-#dat2020 = filter(dat2020, geo != "EU27_2020", geo != "EA19", geo != "EL")
-#dat2020 = filter(dat2020, sex == "T")
+dat2020 = get_eurostat("tps00002", type = "code", filters = list(time = 2020), time_format = "num")
+dat2020 = filter(dat2020, geo != "EU27_2020", geo != "EA19", geo != "EL")
+dat2020 = filter(dat2020, sex == "T")
 # Rok 2030
-#dat2030 = get_eurostat("tps00002", type = "code", filters = list(time = 2030), time_format = "num")
-#dat2030 = filter(dat2030, geo != "EU27_2020", geo != "EA19", geo != "EL")
-#dat2030 = filter(dat2030, sex == "T")
+dat2030 = get_eurostat("tps00002", type = "code", filters = list(time = 2030), time_format = "num")
+dat2030 = filter(dat2030, geo != "EU27_2020", geo != "EA19", geo != "EL")
+dat2030 = filter(dat2030, sex == "T")
 # Rok 2040
-#dat2040 = get_eurostat("tps00002", type = "code", filters = list(time = 2040), time_format = "num")
-#dat2040 = filter(dat2040, geo != "EU27_2020", geo != "EA19", geo != "EL")
-#dat2040 = filter(dat2040, sex == "T")
+dat2040 = get_eurostat("tps00002", type = "code", filters = list(time = 2040), time_format = "num")
+dat2040 = filter(dat2040, geo != "EU27_2020", geo != "EA19", geo != "EL")
+dat2040 = filter(dat2040, sex == "T")
 # Rok 2050
-#dat2050 = get_eurostat("tps00002", type = "code", filters = list(time = 2050), time_format = "num")
-#dat2050 = filter(dat2050, geo != "EU27_2020", geo != "EA19", geo != "EL")
-#dat2050 = filter(dat2050, sex == "T")
+dat2050 = get_eurostat("tps00002", type = "code", filters = list(time = 2050), time_format = "num")
+dat2050 = filter(dat2050, geo != "EU27_2020", geo != "EA19", geo != "EL")
+dat2050 = filter(dat2050, sex == "T")
 # Rok 2060
-#dat2060 = get_eurostat("tps00002", type = "code", filters = list(time = 2060), time_format = "num")
-#dat2060 = filter(dat2060, geo != "EU27_2020", geo != "EA19", geo != "EL")
-#dat2060 = filter(dat2060, sex == "T")
+dat2060 = get_eurostat("tps00002", type = "code", filters = list(time = 2060), time_format = "num")
+dat2060 = filter(dat2060, geo != "EU27_2020", geo != "EA19", geo != "EL")
+dat2060 = filter(dat2060, sex == "T")
 # Rok 2070
-#dat2070 = get_eurostat("tps00002", type = "code", filters = list(time = 2020), time_format = "num")
-#dat2070 = filter(dat2020, geo != "EU27_2020", geo != "EA19", geo != "EL")
-#dat2070 = filter(dat2020, sex == "T")
+dat2070 = get_eurostat("tps00002", type = "code", filters = list(time = 2020), time_format = "num")
+dat2070 = filter(dat2020, geo != "EU27_2020", geo != "EA19", geo != "EL")
+dat2070 = filter(dat2020, sex == "T")
 # Rok 2080
-#dat2080 = get_eurostat("tps00002", type = "code", filters = list(time = 2080), time_format = "num")
-#dat2080 = filter(dat2080, geo != "EU27_2020", geo != "EA19", geo != "EL")
-#dat2080 = filter(dat2080, sex == "T")
+dat2080 = get_eurostat("tps00002", type = "code", filters = list(time = 2080), time_format = "num")
+dat2080 = filter(dat2080, geo != "EU27_2020", geo != "EA19", geo != "EL")
+dat2080 = filter(dat2080, sex == "T")
 # Rok 2090
-#dat2090 = get_eurostat("tps00002", type = "code", filters = list(time = 2090), time_format = "num")
-#dat2090 = filter(dat2090, geo != "EU27_2020", geo != "EA19", geo != "EL")
-#dat2090 = filter(dat2090, sex == "T")
+dat2090 = get_eurostat("tps00002", type = "code", filters = list(time = 2090), time_format = "num")
+dat2090 = filter(dat2090, geo != "EU27_2020", geo != "EA19", geo != "EL")
+dat2090 = filter(dat2090, sex == "T")
 # Rok 2100
-#dat2100 = get_eurostat("tps00002", type = "code", filters = list(time = 2100), time_format = "num")
-#dat2100 = filter(dat2100, geo != "EU27_2020", geo != "EA19", geo != "EL")
-#dat2100 = filter(dat2100, sex == "T")
+dat2100 = get_eurostat("tps00002", type = "code", filters = list(time = 2100), time_format = "num")
+dat2100 = filter(dat2100, geo != "EU27_2020", geo != "EA19", geo != "EL")
+dat2100 = filter(dat2100, sex == "T")
 
 #########################################################
 
@@ -124,18 +124,15 @@ wyciag_danych = function(x) {
   dane = round(sum(dane) / 1000000)
 }
 
-lud = wyciag_danych(2020)
-lud
-
-ludnosc_2020 = wyciag_danych(dat2020)
-ludnosc_2030 = wyciag_danych(dat2030)
-ludnosc_2040 = wyciag_danych(dat2040)
-ludnosc_2050 = wyciag_danych(dat2050)
-ludnosc_2060 = wyciag_danych(dat2060)
-ludnosc_2070 = wyciag_danych(dat2070)
-ludnosc_2080 = wyciag_danych(dat2080)
-ludnosc_2090 = wyciag_danych(dat2090)
-ludnosc_2100 = wyciag_danych(dat2100)
+ludnosc_2020 = wyciag_danych(2020)
+ludnosc_2030 = wyciag_danych(2030)
+ludnosc_2040 = wyciag_danych(2040)
+ludnosc_2050 = wyciag_danych(2050)
+ludnosc_2060 = wyciag_danych(2060)
+ludnosc_2070 = wyciag_danych(2070)
+ludnosc_2080 = wyciag_danych(2080)
+ludnosc_2090 = wyciag_danych(2090)
+ludnosc_2100 = wyciag_danych(2100)
 
 Liczba_ludnosci = data.frame(Rok = c(2020, 2030, 2040, 2050, 2060, 2070, 2080, 2090, 2100),
                              populacja = c(ludnosc_2020, ludnosc_2030,ludnosc_2040, ludnosc_2050, ludnosc_2060, ludnosc_2070, ludnosc_2080, ludnosc_2090, ludnosc_2100))
@@ -144,11 +141,10 @@ Liczba_ludnosci
 
 # Wykres
 
-library(ggplot2)
 
 ggplot(Liczba_ludnosci, aes(x = Rok, y = populacja)) +
   geom_line(stat = "identity", colour = "lightblue", size = 2) +
-  scale_y_continuous(limits = c(200, 600),
+  scale_y_continuous(limits = c(300, 550),
                      breaks = c(300, 450, 600)) +
   labs(title = "Zmiana liczby ludnosci w Europie w latach 2020-2100",
        y = "Populacja w milionach") +
